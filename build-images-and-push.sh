1d7f85c4f27e3dfe92b95f7eb71bc2ed2399eeff
Elias Stalder <elias.stalder@zuhlke.com>
2018-06-05 10:27:29 +0200

    added script to build and push docker images to aws ecr (only works when logged in with docker)

---
+++ build-images-and-push.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+(
+    docker build -t sre-logstash ./containers/logstash/ \
+        && docker tag sre-logstash:latest 870594606895.dkr.ecr.eu-west-2.amazonaws.com/sre-logstash \
+        && docker push 870594606895.dkr.ecr.eu-west-2.amazonaws.com/sre-logstash \
+    && echo "pushed logstash image"
+) &
+
+(
+    docker build -t sre-elasticsearch ./containers/elasticsearch/ \
+        && docker tag sre-elasticsearch:latest 870594606895.dkr.ecr.eu-west-2.amazonaws.com/sre-elasticsearch \
+        && docker push 870594606895.dkr.ecr.eu-west-2.amazonaws.com/sre-elasticsearch \
+        && echo "pushed elasticsearch image"
+)&
+
+(
+    docker build -t sre-kibana ./containers/kibana/ \
+        && docker tag sre-kibana:latest 870594606895.dkr.ecr.eu-west-2.amazonaws.com/sre-kibana \
+        && docker push 870594606895.dkr.ecr.eu-west-2.amazonaws.com/sre-kibana \
+        && echo "pushed kibana image"
+)&
+
+wait
