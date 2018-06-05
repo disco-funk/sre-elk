f731bf1933a1d83bac0dde60033bd773adf8abec
Rik Howard <rik.howard@zuhlke.com>
2018-06-05 13:55:27 +0100

    interrim commitment

--- tear-down.sh
+++ tear-down.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-kubectl delete deployment sre-kibana
-kubectl delete deployment sre-elasticsearch
-# kubectl delete deployment sre-logstash
+kubectl delete deployment sre-elk-kibana
+kubectl delete deployment sre-elk-elasticsearch
+# kubectl delete deployment sre-elk-logstash
 
-kubectl delete service sre-kibana
-kubectl delete service sre-elasticsearch
-#kubectl delete service sre-logstash
+kubectl delete service sre-elk-kibana
+kubectl delete service sre-elk-elasticsearch
+#kubectl delete service sre-elk-logstash
 
-kubectl delete ds sre-logstash
+kubectl delete ds sre-elk-logstash
 
-kubectl delete pods $(kubectl get pods | egrep sre-kibana | cut -d ' ' -f 1)
-kubectl delete pods $(kubectl get pods | egrep sre-elasticsearch | cut -d ' ' -f 1)
-for lsPod in $(kubectl get pods | egrep sre-logstash | cut -d ' ' -f 1)
+kubectl delete pods $(kubectl get pods | egrep sre-elk-kibana | cut -d ' ' -f 1)
+kubectl delete pods $(kubectl get pods | egrep sre-elk-elasticsearch | cut -d ' ' -f 1)
+for lsPod in $(kubectl get pods | egrep sre-elk-logstash | cut -d ' ' -f 1)
 do kubectl delete pods ${lsPod}
 done
