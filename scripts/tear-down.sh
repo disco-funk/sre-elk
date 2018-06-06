5a0fa6e28dd5449f6fb8d59379ec79530005e48c
Elias Stalder <elias.stalder@zuhlke.com>
2018-06-06 14:42:31 +0200

    removed logstash from setup and teardown scripts as it is deployed with microservice

--- scripts/tear-down.sh
+++ scripts/tear-down.sh
@@ -2,7 +2,7 @@
 
 kubectl delete deployment sre-elk-kibana
 kubectl delete deployment sre-elk-elasticsearch
-kubectl delete deployment sre-elk-logstash
+#kubectl delete deployment sre-elk-logstash
 
 kubectl delete service sre-elk-kibana
 kubectl delete service sre-elk-elasticsearch
