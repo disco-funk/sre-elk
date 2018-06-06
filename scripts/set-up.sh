0bd74bd884cf89ccb12b92118d2f37817d29cadd
Elias Stalder <elias.stalder@zuhlke.com>
2018-06-06 14:24:47 +0200

    trying to fix logstash

--- scripts/set-up.sh
+++ scripts/set-up.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 work=$(dirname $0)
-kubectl create -f ${work}/templates/elasticsearch-deployment.yml
-kubectl create -f ${work}/templates/kibana-deployment.yml
-kubectl create -f ${work}/templates/elasticsearch-service.yml
-kubectl create -f ${work}/templates/kibana-service.yml
-kubectl create -f ${work}/templates/logstash-daemonset.yml
+kubectl create -f ${work}/../templates/elasticsearch-deployment.yml
+kubectl create -f ${work}/../templates/kibana-deployment.yml
+kubectl create -f ${work}/../templates/elasticsearch-service.yml
+kubectl create -f ${work}/../templates/kibana-service.yml
+kubectl create -f ${work}/../templates/logstash-deployment.yml
--- scripts/tear-down.sh
+++ scripts/tear-down.sh
@@ -2,8 +2,7 @@
 
 kubectl delete deployment sre-elk-kibana
 kubectl delete deployment sre-elk-elasticsearch
+kubectl delete deployment sre-elk-logstash
 
 kubectl delete service sre-elk-kibana
 kubectl delete service sre-elk-elasticsearch
-
-kubectl delete ds sre-elk-logstash
